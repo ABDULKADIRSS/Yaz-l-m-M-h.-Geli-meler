f7daee5efdfbd608646de77c330ae6f7d326ff10
ABDULKADIRSS <tomokadir44@gmail.com>
2024-11-11 21:18:03 +0300

    Get endpointi eklenmiştir.

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(name)
+
+@app.route("/add", methods=["GET"])
+def add_numbers():
+    num1 = int(request.args.get("num1", 0))
+    num2 = int(request.args.get("num2", 0))
+    result = num1 + num2
+    return jsonify({"result": result})
 
 if name == "main":
     app.run(port=44)
